b0a9bd29af16f82af0a3b8d2736a7875c18cfb61
MixDream <max_juldashev@mail.ru>
2024-12-02 21:28:59 +0300

    s

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+# Функциональное представление
+def functional_view(request):
+    return render(request, 'second_task/func_template.html')
+
+# Классовое представление
+class ClassBasedView(View):
+    def get(self, request):
+        return render(request, 'second_task/class_template.html')
