2af182438729bd6f75710f4d2c351c30cb8810b1
MixDream <max_juldashev@mail.ru>
2024-12-12 22:59:15 +0300

    s

--- UrbanDjango/task5/forms.py
+++ UrbanDjango/task5/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-
+from django.core.validators import RegexValidator
 class UserRegister(forms.Form):
     username = forms.CharField(max_length=30, label="Введите логин")
     password = forms.CharField(min_length=8, widget=forms.PasswordInput, label="Введите пароль")
     repeat_password = forms.CharField(min_length=8, widget=forms.PasswordInput, label="Повторите пароль")
-    age = forms.IntegerField(max_value=120, min_value=0, label="Введите свой возраст")
+
+    # Добавляем валидатор для возраста
+    age_validator = RegexValidator(regex=r'^\d{1,3}$', message="Введите корректный возраст (0-120).")
+    age = forms.IntegerField(
+        max_value=120,
+        min_value=0,
+        label="Введите свой возраст",
+        validators=[age_validator]
+    )
